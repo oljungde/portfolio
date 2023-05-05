b24c0af28bd46023f4f0210da726af77b214f360
oljungde <oljung@posteo.de>
2023-05-05 20:46:17 +0200

    bug fix links

--- src/app/header-offset.directive.spec.ts
+++ src/app/header-offset.directive.spec.ts
@@ -1,8 +1,9 @@
+import { ElementRef } from '@angular/core';
 import { HeaderOffsetDirective } from './header-offset.directive';
 
 describe('HeaderOffsetDirective', () => {
   it('should create an instance', () => {
-    const directive = new HeaderOffsetDirective();
+    const directive = new HeaderOffsetDirective(new ElementRef(null));
     expect(directive).toBeTruthy();
   });
 });
